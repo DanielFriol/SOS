70fa153e910c19dfaf3e7d2bdf38e06a01c359f3
luizsantana725 <luizsantana725@gmail.com>
2018-10-12 15:43:49 -0300

    att

---
+++ trab1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void Saida (/*parametro nome do arquivo*/)  {
+	FILE * arq;
+	arq = fopen(fname,"w");
+}
+
+void numArq (string argv) {
+	int arqs  = argv - 2;
+}
+
+int main () {
+	
+}
