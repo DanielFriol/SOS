0283a7f7aac129debf6a20d3598ca27eb3fcf1c9
luizsantana725 <luizsantana725@gmail.com>
2018-10-12 16:46:51 -0300

    att

--- trab1.c
+++ trab1.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void Saida (/*parametro nome do arquivo*/)  {
+typedef struct num {
+	int nums;
+}nums;
+
+typedef struct arqs {
+	arq * nums[];
+	int top;
+}arq;
+
+arq.top = 0;
+
+num * newNum (int num) {
+	num * n = (num *) malloc (sizeof(num));
+	n->nums = num;
+	return n;
+} 
+
+void Saida (string argv[], arq * a)  {
 	FILE * arq;
-	arq = fopen(fname,"w");
+	arq = fopen(argv[argc-1],"w");
+	fprintf (arq, "%d", a->top);
+	for (int i=0; i<a->top; i++) {
+		fprintf(arq, "%d"; a->nums[i]);
+	}
+	fclose(arq);
 }
 
-void numArq (string argv) {
-	int arqs  = argv - 2;
+void leArq (int argc, string argv[]) {
+	FILE * arq;
+	int num[];
+	for (int i=2; i<(argc-2); i++) {
+		arq = fopen("argv[i]", "r");
+		arqs  * arq = (arqs *) malloc(sizeof(arqs));
+		fscanf  (argv[i], "%d", &arq->top);
+		for (int x=0; x<arq->top; x++) {
+			fscanf (argv[i], "%d", &num);
+			arq->nums = newNum(num);
+		}
+	}
+	fclose(arq);
 }
 
-int main () {
+int main (int argc, string * argv[]) {
 	
 }
