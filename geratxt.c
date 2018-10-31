096e51912697547d2a4a8a00c760e04699f90609
Daniel Friol <danielfriol@gmail.com>
2018-10-30 22:04:41 -0300

    Arrumado gerador de arquivos e gerados mais .in

--- geratxt.c
+++ geratxt.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #include <time.h>
 
 
 int main(){
-FILE * fp = fopen("arq1.in","w");
+FILE * fp = fopen("arq5.in","w");
 srand(time(NULL));
 int n = rand();
-
 int cont=0;
 for(cont=0;cont<n;cont++){
-    srand(time(NULL));
-    fprintf(fp,"%d",rand());
+
+    fprintf(fp,"%d\n",rand());
 
 
 }
 
+return 0;
 
 }
