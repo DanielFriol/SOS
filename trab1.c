473c692b97d88a77b7d12135403768980d89b0cd
luizsantana725 <luizsantana725@gmail>
2018-10-30 21:26:37 -0300

    correcao de alguns bugs, porem esta dando o erro de SEGMENTATON FAULT. criei um arquivo teste.txt no LInux para teste de arquivo.

--- trab1.c
+++ trab1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAMANHOMAX 1000
 
 typedef struct num {
 	int nums;
 }nums;
 
 typedef struct arqs {
-	nums * nums[200];
+	nums * nu[200];
 	int top;
 }arq;
 
@@ -17,36 +18,47 @@ nums * newNum (int num) {
 	return n;
 } 
 
-void Saida (char * argv[], int argc, arq * a)  {
+void Saida (int argc, char * argv[], arq * a)  {
 	FILE * fp;
-	fp = fopen(argv[argc-1],"w");
+	fp = fopen(argv[argc],"w");
 	fprintf (fp, "%d", a->top);
 	int i;
 	for (i=0; i<a->top; i++) {
-		fprintf(fp, "%d", a->nums[i]);
+		fprintf(fp, "%d", a->nu[i]);
 	}
 	fclose(fp);
 }
 
 void leArq (int argc, char *  argv[]) {
 	FILE * fp;
-	int num[200];
+	int * num;
 	int i, x;
-	for (i=1; i<(argc-2); i++) {
+
+        num = calloc(TAMANHOMAX,sizeof(int));
+
+	for (i=1; i<(argc-1); i++) {
 		fp = fopen(argv[i], "r");
 		arq  * a = (arq *) malloc(sizeof(arq));
 		fscanf  (fp, "%d", &a->top);
 		for (x=0; x<a->top; x++) {
-			fscanf (fp, "%d", &num);
-			a->nums[x] = newNum(num[x]);
+			fscanf (fp, "%d", &num[x]);
+			a->nu[x] = newNum(num[x]);
+                        a->top++;
 		}
-                free(a);
+                Saida (argc, argv, a);
+                //free(a);
 	}
 	fclose(fp);
 }
 
 int main (int argc, char * argv[]) {
-	leArq (argc, argv);
-	arq  * a = (arq *) malloc(sizeof(arq));
-	Saida (argv, argc, a);
+	int numarq=0;
+        char * arquis[100];
+        for (int i=1; i < (argc-1); i++) {
+                strcpy(arquis[numarq], argv[i]);
+                numarq++;
+        }
+
+        leArq (numarq, arquis);
+        
 }
